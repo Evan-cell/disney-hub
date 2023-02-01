08c34efdbe6902ee853255f5c37f8acd5f5f138b
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-02-01 10:23:00 +0300

    configuring fire-base

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA8_wPwPqfN6RrYds8WJSKPhKGOEnLm3K8",
+    authDomain: "disney-build-4e805.firebaseapp.com",
+    projectId: "disney-build-4e805",
+    storageBucket: "disney-build-4e805.appspot.com",
+    messagingSenderId: "363992965019",
+    appId: "1:363992965019:web:14a1355582b06cead60b3e",
+    measurementId: "G-M1KNJYS3R3"
+  };
