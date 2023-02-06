a6eabf0a348e884caaa221a7eb8834265b767f60
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-02-06 09:11:30 +0300

    recommendations,originals,disney plus and trending

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import MovieSlice from '../features/movies/MovieSlice';
 import userReducer from "../features/user/UserSlice";
+
 export default configureStore({
     reducer: {
         user: userReducer,
+        movie: MovieSlice,
     },
   })
--- src/features/movies/MovieSlice.js
+++ src/features/movies/MovieSlice.js
@@ -1,3 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initil
+const initialState ={
+    recommend :'null',
+    newDisney: 'null',
+    originals: 'null',
+    trending: 'null',
+}
+const movieSlice = createSlice({
+    name: 'movie',
+    initialState,
+    reducers: {
+        setMovies: (state, action) =>{
+            state.recommend = action.payload.recommend
+            state.newDisney = action.payload.newDisney
+            state.originals = action.payload.original
+            state.trending = action.payload.trending
+        }
+    }
+})
+export const {setMovies} = movieSlice.actions
+
+export const selectRecommend = state => state.movie.recommend
+export const selectneNewDisney= state => state.movie.newDisney
+export const selectOriginals = state => state.movie.originals
+export const selectTrending = state => state.movie.trending
+
+export default movieSlice.reducer
