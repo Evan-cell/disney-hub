985eb1a09cc5a9bb3ccf77962486a7eab6efdf6f
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-02-06 15:37:03 +0300

    posted detail data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter  as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+import Details from './components/Details';
 import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -16,7 +17,9 @@ function App() {
                 <Route exact path ='/home'>
                     <Home />
                 </Route>
-                <Route exact path='/detail/:id'></Route>
+                <Route exact path='/detail/:id'>
+                  <Details/>
+                </Route>
             </Switch>
         </Router>
        
--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -14,7 +14,7 @@ function Recommendations() {
           movies.map((movie, key) => (
             <Wrap key={key}>
               {movie.id}
-              <Link to={`/detail/` + movie.id}>
+              <Link to={`/details/` + movie.id}>
                 <img src={movie.cardImg} alt={movie.title} />
               </Link>
             </Wrap>
